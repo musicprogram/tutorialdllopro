956ab9c8f01690f221a82b7fdcf669eb4f3e3327
Juan H <juan-o-henao@hotmail.es>
2014-08-09 15:30:49 -0500

    Ruteadores para urls personalizadas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+
+root  'products#index'
+
   resources :departments
 
+
+
+
   resources :products
+    get 'Nuevo_Producto'=> 'products#new' , as:'nuevo_prod'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
